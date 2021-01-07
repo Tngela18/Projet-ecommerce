d633289572e7020e69eee79a99ed47eba428d4b5
a <a@DESKTOP-TSBIQ6H>
2021-01-07 20:57:28 +0100

    First commit uploading all files

---
+++ VenteService/src/test/java/com/yes/VenteService/VenteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yes.VenteService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
